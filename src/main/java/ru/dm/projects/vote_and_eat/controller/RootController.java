d8317205dc972da684fbac7d83915ac9e7d39244
Dmitriy Kurochkin <kurochkin.dm@gmail.com>
2021-10-09 18:19:04 +0300

    attempt 6

---
+++ src/main/java/ru/dm/projects/vote_and_eat/controller/RootController.java
@@ -0,0 +1,15 @@
+package ru.dm.projects.vote_and_eat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Controller
+public class RootController {
+    @GetMapping()
+    public void redirect(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/v4e/swagger-ui.html");
+    }
+}
